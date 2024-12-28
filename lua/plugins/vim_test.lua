245b0e44f83a1833a788d55f0ba230f6ee0e509b
Tim Hrovat <tim.hrovat@easistent.com>
2024-12-28 11:34:12 +0100

    fixed vimtest

--- lua/plugins/vim_test.lua
+++ lua/plugins/vim_test.lua
@@ -6,4 +6,6 @@ return {
     vim.keymap.set("n", "<leader>tn", ":TestNearest<CR>"),
     vim.keymap.set("n", "<leader>T", ":TestFile<CR>"),
     vim.keymap.set("n", "<leader>a", ":TestSuite<CR>"),
+    vim.cmd("let test#strategy = 'vimux'"),
+    vim.cmd("let test#php#phpunit#executable = 'docker exec -it evrtec.web php artisan test --config=phpunit.xml'")
 }
